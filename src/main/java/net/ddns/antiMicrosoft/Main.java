94de04680c201aa77457f3ba9d084fba207d5ef9
Lett <hatsunemiku8726@gmail.com>
2024-08-30 12:32:11 -0400

    Testing preparation

--- src/main/java/net/ddns/antiMicrosoft/Main.java
+++ src/main/java/net/ddns/antiMicrosoft/Main.java
@@ -1,5 +1,20 @@
 package main.java.net.ddns.antiMicrosoft;
 
-public class Main {
+import arc.Events;
+import arc.util.Http;
+import arc.util.Log;
+import mindustry.game.EventType;
+import mindustry.mod.*;
 
+public class Main extends Plugin {
+    public final String url = "https://azure-ip-lookup.azurewebsites.net/api/ipAddress?ipOrDomain=104.45.231.79";
+
+    @Override
+    public void init() {
+        Events.on(EventType.PlayerConnect.class, event -> {
+            Log.info(1);
+            Http.HttpRequest result = Http.get(url);
+            Log.info(result.content);
+        });
+    }
 }
