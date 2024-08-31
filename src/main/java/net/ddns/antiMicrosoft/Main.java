63921fa959fb8e1d932c92a0eb1942e4ccece3b2
Lett <lett@ubuntu-23.10-desktop>
2024-08-30 21:30:55 -0400

    Made it actually functional.

--- src/main/java/net/ddns/antiMicrosoft/Main.java
+++ src/main/java/net/ddns/antiMicrosoft/Main.java
@@ -1,20 +1,41 @@
 package main.java.net.ddns.antiMicrosoft;
 
 import arc.Events;
-import arc.util.Http;
 import arc.util.Log;
 import mindustry.game.EventType;
 import mindustry.mod.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class Main extends Plugin {
-    public final String url = "https://azure-ip-lookup.azurewebsites.net/api/ipAddress?ipOrDomain=104.45.231.79";
+    // Kick duration in minutes
+    Integer kickDurationMinutes = 120;
 
     @Override
     public void init() {
         Events.on(EventType.PlayerConnect.class, event -> {
-            Log.info(1);
-            Http.HttpRequest result = Http.get(url);
-            Log.info(result.content);
+            try {
+                URL url = new URL(String.format("https://www.azurespeed.com/api/ipAddress?ipOrDomain=%s",
+                        event.player.ip()));
+                URLConnection connection = url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                if (bufferedReader.readLine().length() <= 2) {
+                    return;
+                }
+                event.player.kick("Using Azure servers to connect to the mindustry.ddns.net network is not" +
+                        " permitted. If you believe that this was a mistake, then please contact [#ff5a00]bndlett[] or" +
+                        " a similar staff member (i.e. [#ff3f00]xasmedy[] or [#d7342a]chefe[]) via our Discord server.");
+
+            } catch (IOException e) {
+                Log.warn(String.format("An IOException has occurred: %s", e.getMessage()));
+                Log.warn("Source: antiMicrosoft");
+            }
         });
     }
 }
